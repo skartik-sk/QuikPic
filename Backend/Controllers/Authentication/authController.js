ff63b5e38ad0ff9b67213e211156c0c26eeba6d8
Singupalli Kartik <skartik1706@gmail.com>
2024-02-08 17:00:01 +0530

    Update .env file and authController.js

--- Backend/Controllers/Authentication/authController.js
+++ Backend/Controllers/Authentication/authController.js
@@ -65,56 +65,7 @@ export const forgotPassword = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-
-    export const signup = async (req, res) => {
-      try {
-        const { username, email, password } = req.body;
-        const user = await User.findOne({ email });
-
-export const forgotPassword = async (req, res) => {
-  try {
-    const { email } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // Generate a reset token
-    const token = jwt.sign({ _id: user._id }, process.env.RESET_PASSWORD_KEY, { expiresIn: '30m' });
-    
-    // Send the reset token to the user's email
-    const transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
-
-    const mailOptions = {
-      from: process.env.EMAIL_USERNAME,
-      to: email,
-      subject: 'Password Reset Link',
-      html: `<p>Please click this <a href="${process.env.CLIENT_URL}/reset-password/${token}">link</a> to reset your password.</p>`
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.log(error);
-        return res.status(500).json({ message: "Failed to send reset email" });
-      } else {
-        console.log('Email sent: ' + info.response);
-        return res.status(200).json({ message: "Reset email sent successfully" });
-      }
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
-
-    export const signup = async (req, res) => {
+export const signup = async (req, res) => {
       try {
         const { username, email, password } = req.body;
         const user = await User.findOne({ email });
