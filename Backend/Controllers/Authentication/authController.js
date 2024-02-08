53e01e3598220bd4d78144535ea0c29b88609533
Singupalli Kartik <skartik1706@gmail.com>
2024-02-08 16:43:47 +0530

    Refactor authentication controller and add signup and logout functionality

--- Backend/Controllers/Authentication/authController.js
+++ Backend/Controllers/Authentication/authController.js
@@ -1,6 +1,6 @@
 import User from "../../Models/userModel.js";
-import nodemailer from 'nodemailer';
-import jwt from 'jsonwebtoken';
+import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
 
 export const login = async (req, res) => {
   try {
@@ -33,7 +33,7 @@ export const forgotPassword = async (req, res) => {
     }
 
     // Generate a reset token
-    const token = jwt.sign({ _id: user._id }, process.env.SECREAT_KEY, { expiresIn: '30m' });
+    const token = jwt.sign({ _id: user._id }, process.env.RESET_PASSWORD_KEY, { expiresIn: '30m' });
     
     // Send the reset token to the user's email
     const transporter = nodemailer.createTransport({
@@ -48,7 +48,7 @@ export const forgotPassword = async (req, res) => {
       from: process.env.EMAIL_USERNAME,
       to: email,
       subject: 'Password Reset Link',
-      html: `<p>Please click this <a href="ThisIsYourReset/reset-password/${user._id}/${token}">link</a> to reset your password.</p>`
+      html: `<p>Please click this <a href="${process.env.CLIENT_URL}/reset-password/${token}">link</a> to reset your password.</p>`
     };
 
     transporter.sendMail(mailOptions, (error, info) => {
@@ -71,24 +71,70 @@ export const forgotPassword = async (req, res) => {
         const { username, email, password } = req.body;
         const user = await User.findOne({ email });
 
-        if (user) {
-          return res.status(409).json({ message: "User already exists" });
-        }
-        const newUser = new User({ username, email, password });
-        await newUser.save();
+    if (user) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+    const newUser = new User({ username, email, password });
+    await newUser.save();
 
-        const token = await newUser.generateAuthToken();
+    const token = await newUser.generateAuthToken();
 
-        res
-          .status(201)
-          .cookie("access_token", token)
-          .json({ message: "Signup successful" });
-      } catch (error) {
-        res.status(500).json({ message: error });
+    res
+      .status(201)
+      .cookie("access_token", token)
+      .json({ message: "Signup successful" });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+};
+export const logout = (req, res) => {
+  res.clearCookie("access_token");
+
+  res.status(200).json({ message: "logout successful" });
+};
+export const forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Generate a reset token
+    const token = jwt.sign({ _id: user._id }, process.env.RESET_PASSWORD_KEY, {
+      expiresIn: "30m",
+    });
+
+    // Send the reset token to the user's email
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USERNAME,
+      to: email,
+      subject: "Password Reset Link",
+      html: `<p>Please click this <a href="${process.env.CLIENT_URL}/reset-password/${token}">link</a> to reset your password.</p>`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Failed to send reset email" });
+      } else {
+        console.log("Email sent: " + info.response);
+        return res
+          .status(200)
+          .json({ message: "Reset email sent successfully" });
       }
-    };
-    export const logout = (req, res) => {
-      res.clearCookie("access_token");
-
-      res.status(200).json({ message: "logout successful" });
-    };
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
