dd591a6f17ea417d5772b61c529a58babcac3ffe
Singupalli Kartik <skartik1706@gmail.com>
2024-02-16 23:20:18 +0530

    Refactor savedPostController.js

--- Backend/Controllers/post/savedPostController.js
+++ Backend/Controllers/post/savedPostController.js
@@ -1,25 +1,63 @@
 import UserModel from "../../Models/userModel.js";
 
-// Save a post
+// // Save a post
+// export const savePost = async (req, res) => {
+//   try {
+//     const { username, post } = req.body;
+//     const savedPost = await UserModel.create({ username, post });
+//     res.status(201).json(savedPost);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Server Error' });
+//   }
+// };
+
+// // Retrieve saved posts for a user
+// export const getSavedPosts = async (req, res) => {
+//   try {
+//     const userId = req.params.id;
+//     const savedPosts = await UserModel.find({ user: userId }).populate('post');
+//     res.json(savedPosts);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ message: 'Server Error' });
+//   }
+// };
 export const savePost = async (req, res) => {
   try {
-    const { username, post } = req.body;
-    const savedPost = await UserModel.create({ username, post });
-    res.status(201).json(savedPost);
+    const postId = req.body.postId;
+    const userId = res.user.id._id;
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    if (user.savedPosts.includes(postId)) {
+      
+      user.savedPosts = user.savedPosts.filter((savedPost) => savedPost !== postId);
+    } else {
+      
+      user.savedPosts.push(postId);
+    }
+    await user.save();
+    res.status(200).json({ message: "Post saved successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+    res.status(500).json({ message: "Server Error" });
   }
 };
 
-// Retrieve saved posts for a user
 export const getSavedPosts = async (req, res) => {
   try {
-    const userId = req.params.id;
-    const savedPosts = await UserModel.find({ user: userId }).populate('post');
-    res.json(savedPosts);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: 'Server Error' });
+    const userId = res.user.id._id;
+    const user = await UserModel.findById(userId)
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user.savedPosts);
   }
-};
+  catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+
+}
