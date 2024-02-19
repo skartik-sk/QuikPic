228966cc04205cadddb561baa8eb36be3f94dfd9
Singupalli Kartik <skartik1706@gmail.com>
2024-02-19 15:43:19 +0530

    Add Tailwind CSS and Next UI dependencies

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,65 @@
+import React from "react";
+import {
+  Spinner,
+  RadioGroup,
+  Radio,
+  Switch,
+  Card,
+  CardFooter,
+  Image,
+  Button,
+} from "@nextui-org/react";
 
-import './App.css'
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
-function App() {
+export default function App() {
+  const colors = ["primary", "secondary", "success", "warning", "danger"];
 
   return (
     <>
-      <Card className="py-4">
-      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-        <p className="text-tiny uppercase font-bold">Daily Mix</p>
-        <small className="text-default-500">12 Tracks</small>
-        <h4 className="font-bold text-large">Frontend Radio</h4>
-      </CardHeader>
-      <CardBody className="overflow-visible py-2">
+      <div className=" bg-black">hi</div>
+      <RadioGroup
+        label="Select your favorite city"
+        color="secondary"
+        defaultValue="london"
+      >
+        <Radio value="buenos-aires">Buenos Aires</Radio>
+        <Radio value="sydney">Sydney</Radio>
+        <Radio value="san-francisco">San Francisco</Radio>
+        <Radio value="london">London</Radio>
+        <Radio value="tokyo">Tokyo</Radio>
+      </RadioGroup>
+      <Spinner />
+      <Switch defaultSelected size="lg" color="secondary">
+        Dark mode
+      </Switch>
+      <div className=" flex  gap-4">
+        <Spinner label="Default" color="default" labelColor="foreground" />
+        <Spinner label="Primary" color="primary" labelColor="primary" />
+        <Spinner label="Secondary" color="secondary" labelColor="secondary" />
+        <Spinner label="Success" color="success" labelColor="success" />
+        <Spinner label="Warning" color="warning" labelColor="warning" />
+        <Spinner label="Danger" color="danger" labelColor="danger" />
+      </div>
+      <Card isFooterBlurred radius="lg" className="border-none">
         <Image
-          alt="Card background"
-          className="object-cover rounded-xl"
-          src="https://images.unsplash.com/photo-1708022799750-afbe4be26f73?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8"
-          width={270}
+          alt="Woman listing to music"
+          className="object-cover"
+          height={200}
+          src="https://images.unsplash.com/photo-1682687220945-922198770e60?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
+          width={200}
         />
-      </CardBody>
-    </Card>
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny text-white/80">Available soon.</p>
+          <Button
+            className="text-tiny text-white bg-black/20"
+            variant="flat"
+            color="default"
+            radius="lg"
+            size="sm"
+          >
+            Notify me
+          </Button>
+        </CardFooter>
+      </Card>
     </>
-  )
+  );
 }
-
-export default App
--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,10 +1,14 @@
+// tailwind.config.js
+import {nextui} from '@nextui-org/theme'
+
 /** @type {import('tailwindcss').Config} */
-import nextui from "./node_modules/@nextui-org/react";
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+export default  {
+  content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()]
+  plugins: [nextui()],
 };
