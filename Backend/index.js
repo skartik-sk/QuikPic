d3e5405d27336981a0701e7fcde472df1c0d6daa
Singupalli Kartik <skartik1706@gmail.com>
2024-03-14 21:54:50 +0530

    Update CORS origin to use production URL

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookie());
-app.use(cors({  origin:"http://localhost:5173",
+app.use(cors({  origin:"https://quik-pic.vercel.app",
 credentials:true}));
 app.use("/",userRouter);
 app.use("/post",postRouter); 
